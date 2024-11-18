e154f6e0993a93e00485a3f220555ac931360043
outrasilva <jessica.1702@hotmail.com>
2024-11-18 17:33:46 -0300

    Finalização do JavaScript.

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+const container = document.querySelector(".container");
+
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+const qrCodeInput = document.querySelector("#qr-form input");
+
+const qrCodeImg = document.querySelector("#qr-code img");
+
+// Eventos
+
+// Função para gerar o QR Code
+function generateQrCode( ) {
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if (!qrCodeInputValue) return;
+
+    qrCodeBtn.innerHTML = "Generando código...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    qrCodeImg.addEventListener("load", () => {
+        container.classList.add("active");
+        qrCodeBtn.innerHTML = "Código criado!";
+    });
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    generateQrCode( );
+});
+
+// Para o Enter funcionar
+qrCodeInput.addEventListener("keydown", (e) => {
+    if(e.code === "Enter") {
+        generateQrCode( );
+    }
+});
+
+// Limpar área do QR Code
+qrCodeInput.addEventListener("keyup", () => {
+    if (!qrCodeInput.value) {
+        container.classList.remove("active");
+        qrCodeBtn.innerHTML = "Gerar QR Code";
+    }
+});
